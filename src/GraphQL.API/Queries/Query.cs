a9cffbec1ae4bfe96e530f65a3437a9725dbc3f9
Diego Pereira <diego.pereira@farfetch.com>
2021-03-28 20:24:02 +0100

    Add dockerfile and docker-compose

--- src/GraphQL.API/Queries/Query.cs
+++ src/GraphQL.API/Queries/Query.cs
@@ -8,9 +8,15 @@
 
     public class Query
     {
-        public Task<IEnumerable<Product>> GetProductsAsync([Service] IProductRepository productRepository)
-        {
-            return productRepository.GetAllAsync();
-        }
+        public Task<IEnumerable<Product>> GetProductsAsync([Service] IProductRepository productRepository) =>
+            productRepository.GetAllAsync();
+
+        public Task<Product> GetProductById(string id, [Service] IProductRepository productRepository) =>
+            productRepository.GetByIdAsync(id);
+
+        // Add any queries you want here..
+        // - Get categories
+        // - Get products by category
+        // - etc
     }
 }
